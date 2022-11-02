9376c51ca2554556fa975ae73f730d9a439ff388
Carlos <casalgado86@gmail.com>
2022-11-02 10:08:42 -0500

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected } from "./util/interact.js";
+
+const Home = () => {
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  //called only once
+  useEffect(() => {
+    async function fetchWallet() {
+      const { address, status } = await getCurrentWalletConnected();
+      setWallet(address);
+      setStatus(status);
+      console.log("status:", status);
+    }
+    fetchWallet();
+    addWalletListener();
+  }, []);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("Connected");
+        } else {
+          setWallet("");
+          setStatus("Connect to Metamask using the top left button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setWallet(walletResponse.address);
+    setStatus(walletResponse.status);
+  };
+
+  return (
+    <div id="container">
+      <button className="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/util/interact.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "Connected",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status:
+        " You must install Metamask, a virtual Ethereum wallet, in your browser.",
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "Connected",
+        };
+      } else {
+        return {
+          address: "",
+          status: "Connect to Metamask using the top left button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
